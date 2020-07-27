aeea70706e5afa1f62ade5a643ffeb9fb0360024
Jesse Neumann <jesseneumann@gmail.com>
2020-07-27 13:23:12 -0600

    cors

--- app/server.py
+++ app/server.py
@@ -1,5 +1,6 @@
 # import required libraries
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from app.PredictGame import PredictGame
 from app.ListGames import ListGames
@@ -8,10 +9,9 @@ from app.ListGames import ListGames
 app = Flask(__name__)
 api = Api(app)
 
+CORS(app)
 
 # Setup the Api resource routing here
 # Route the URL to the resource
 api.add_resource(PredictGame, '/game')
 api.add_resource(ListGames, '/games')
-
-
